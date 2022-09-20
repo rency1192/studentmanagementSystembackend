2ce4e0b5c33a6898e425e6c7da6ceb3299a38fe1
rency_1192 <20it051@charusat.edu.in>
2022-09-20 12:21:12 +0530

    Added security with custom users jdbc authentication using H2 database queries to add users at runtime.

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,16 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser("admin")
-                .password("pass")
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("user")
-                .password("pass")
-                .roles("USER");
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        auth.jdbcAuthentication().dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser("admin")
+//                .password("pass")
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("user")
+//                .password("pass")
+//                .roles("USER");
+
+
 //        auth.inMemoryAuthentication()
 //                .withUser("admin")
 //                .password("pass")
@@ -40,8 +45,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        public PasswordEncoder getPasswordEncoder() {
-            return NoOpPasswordEncoder.getInstance();
-        }
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
